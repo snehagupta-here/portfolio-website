41bf3ab42e49cc58adc48e2a5244aa5b8bcef2ec
Sneha Gupta <snehagupta7385@gmail.com>
2024-05-12 17:49:58 +0530

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,7 @@ import './App.css';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import HomePage from './Pages/HomePage';
 import AdminHomePage from './Pages/Admin/AdminHomePage';
-import AdminCreate from './components/Admin/AdminCreate';
-import AdminContact from './components/Admin/AdminContact';
-import AdminTestimonial from './components/Admin/AdminTestimonial';
-import AdminService from './components/Admin/AdminService';
-import AdminWork from './components/Admin/AdminWork';
-import AdminHero from './components/Admin/AdminHero';
-import AdminAbout from './components/Admin/AdminAbout';
+
 function App() {
   return (
          <>
@@ -17,13 +11,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} /> 
             <Route path="/admin" element={<AdminHomePage />} /> 
-            <Route path="/admincreate" element={<AdminCreate />} /> 
-            <Route path="/admincontact" element={<AdminContact />} /> 
-            <Route path="/admintestimonial" element={<AdminTestimonial />} /> 
-            <Route path="/adminservice" element={<AdminService />} />
-            <Route path="/adminwork" element={<AdminWork />} />  
-            <Route path="/adminhero" element={<AdminHero />} />  
-            <Route path="/adminabout" element={<AdminAbout />} /> 
+           
           </Routes>
          </Router>
          </>
